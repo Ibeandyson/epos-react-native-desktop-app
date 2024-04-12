e58b34a774523a9a6b25239ecfc39d3dc81fb18c
ibeandyson <ibeandyson123@gmail.com>
2024-04-12 12:09:51 +0100

    worked on till product card

--- src/components/CustomTillProductCard.tsx
+++ src/components/CustomTillProductCard.tsx
@@ -1,17 +1,17 @@
 import { View, Text } from 'react-native';
 import { Card } from 'react-native-paper';
 import CustomButton from './CustomButton';
-import { Svg, Path } from 'react-native-svg';
 
 const CustomTillProductCard = () => {
   return (
     <>
       <Card style={{ backgroundColor: '#ffffff', height: 100 }} mode="elevated">
         <Card.Content style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-          <View style={{flexDirection: "row", justifyContent: }}>
+          <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
             <View style={{ width: 60, height: 66, backgroundColor: 'rgba(217, 211, 222, 1)', borderRadius: 10 }}></View>
-            <View style={{marginLeft: 20}}>
-            <Text>Four Cousins</Text>
+            <View style={{ marginLeft: 20 }}>
+              <Text style={{fontWeight: "700", fontSize: 14, marginTop: 10}}>Four Cousins</Text>
+              <Text style={{fontWeight: "500", fontSize: 11,marginTop: 5 }}>20 Available</Text>
             </View>
           </View>
           <View style={{ justifyContent: 'flex-end', alignItems: 'flex-end' }}>
